5ca4258548f72cc6a37fc7bc8441f7ada20a907c
Peter Birsinger <pbirsinger@gmail.com>
2018-11-29 10:27:42 -0500

    rm middleware

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,6 @@ import * as express from "express";
 import * as path from "path";
 import serverless from "serverless-http";
 
-import renderer from "./middleware/renderer";
-
 const app = express();
 
 app.use(express.static("client/build"));
